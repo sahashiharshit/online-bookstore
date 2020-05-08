6fbcffdcd25b70f22ee37f4a8b70a04d325ab24d
Harshit Sah <harshitsah96@gmail.com>
2020-05-08 11:37:48 +0530

    angular Front end for Online Bookstore

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,15 @@
+export class Book {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+
+
+
+}
