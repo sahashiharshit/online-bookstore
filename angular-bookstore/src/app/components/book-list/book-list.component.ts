a69d7e3a9df1f74a7f1a2094be124cca4ea39a27
harshit sah <harshitsah96@gmail.com>
2020-05-10 20:15:08 +0530

    Created the angular frontend and Connected to the backend succesfully. Rest Api working. Work in progress...

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -8,47 +9,21 @@ import { Book } from 'src/app/common/book';
 })
 export class BookListComponent implements OnInit {
 
-  books: Book[] = [
+  
+  books: Book[];
 
-
-    {
-      sku: "text-100",
-      name: "Python for SAS Users",
-      description: "Business users familiar with Base SAS programming can now learn Python by example. You will learn via examples that map SAS programming constructs and coding patterns into their Python equivalents.",
-      unitPrice: 600,
-      imageUrl: "assets/images/webdevelopment/text-100.jpg",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    },
-    {
-      sku: "text-101",
-      name: "Deep Learning with JavaScript",
-      description: "Deep learning has transformed the fields of computer vision, image processing, and natural language applications.",
-      unitPrice: 500,
-      imageUrl: "assets/images/webdevelopment/text-101.jpg",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    },
-    {
-      sku: "text-102",
-      name: "Clean Ruby",
-      description: "Learn how to make better decisions and write cleaner Ruby code. This book shows you how to avoid messy code that is hard to test",
-      unitPrice: 700,
-      imageUrl: "assets/images/webdevelopment/text-102.jpg",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    }
-  ];
-
-  constructor() { }
+  constructor(private _bookService: BookService) { }
 
   ngOnInit(): void {
+    this.listBooks();
+  }
+  listBooks(){
+
+      this._bookService.getBooks().subscribe(
+
+        data => this.books = data
+        
+      )
   }
 
 }
